89c048557dd04cf1eafe1ae9a8971185bee583be
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-06-13 13:58:42 +0000

    done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function countNum() {
+    const userMin = parseInt(document.getElementById("minNum").value)
+    const userMax = parseInt(document.getElementById("maxNum").value)
+    if (userMin >= userMax) {
+        document.getElementById("output").innerHTML = "Please enter valid numbers"
+    }
+    else {   let counter = userMin
+        let numbers = ""
+        while (counter < userMax) {
+            counter = counter + 1
+            numbers = numbers + counter + "<br>"}
+        document.getElementById("output").innerHTML = "" + numbers }
+}
